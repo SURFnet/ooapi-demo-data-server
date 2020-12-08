986293f502e085f36739612319538ffd319a94ef
Jelmer de Ronde <jelmer.deronde@surf.nl>
2020-12-08 14:42:56 +0100

    Add mechanism to set seeds via environment

--- src/instant_ooapi/core.clj
+++ src/instant_ooapi/core.clj
@@ -2,10 +2,21 @@
   (:require
     [instant-ooapi.system :as system]
     [io.pedestal.log :as log]
-    [integrant.core :as ig]))
+    [integrant.core :as ig]
+    [clojure.data.generators]))
+
+(defn seed
+  []
+  (let [e (System/getenv "SEED")
+        s (try (Integer/parseInt e)
+            (catch Exception _ (rand-int 100000)))]
+    (or s (rand 100000))))
 
 (defn -main
-  [& args]
+  [& _]
   (log/info :instant-ooapi/starting {})
-  (let [system (ig/init (system/prep :prod))]
+  (let [s (seed)
+        system (binding [clojure.data.generators/*rnd* (java.util.Random. s)]
+                 (ig/init (system/prep :prod)))]
+    (log/info :instant-ooapi/seed {:seed s})
     system))
